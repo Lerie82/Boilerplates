5bec74ce646cfe584d7189bb993687f9bb54ca73
lerie82 <ltaylor82619@gmail.com>
2021-11-11 10:45:23 -0700

    WordPress-plugin-basic

---
+++ WordPress-Plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Lerie Taylor
+ * @version 0.0.1
+ */
+/*
+Plugin Name: Lerie
+Plugin URI: http://lerietaylor.com
+Description: A WordPress Plugin by Lerie Taylor
+Author: Lerie Taylor
+Version: 0.0.1
+Author URI: http://lerietaylor.com
+*/
